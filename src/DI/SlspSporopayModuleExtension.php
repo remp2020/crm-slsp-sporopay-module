338692b550d7ba1be60c81fb74700549c4c29b85
Miroslav Svitok <mirosvitok@gmail.com>
2022-08-25 08:28:07 +0000

    Upgrade to PHP 8.0, upgrade to Deployer 7
    - new deploy strategy, project is copied to application servers instead of using NFS
    
    remp/crm#2519

--- src/DI/SlspSporopayModuleExtension.php
+++ src/DI/SlspSporopayModuleExtension.php
@@ -2,10 +2,10 @@
 
 namespace Crm\SlspSporopayModule\DI;
 
-use Kdyby\Translation\DI\ITranslationProvider;
+use Contributte\Translation\DI\TranslationProviderInterface;
 use Nette\DI\CompilerExtension;
 
-final class SlspSporopayModuleExtension extends CompilerExtension implements ITranslationProvider
+final class SlspSporopayModuleExtension extends CompilerExtension implements TranslationProviderInterface
 {
     private $defaults = [];
 
@@ -24,7 +24,7 @@ final class SlspSporopayModuleExtension extends CompilerExtension implements ITr
      * Return array of directories, that contain resources for translator.
      * @return string[]
      */
-    public function getTranslationResources()
+    public function getTranslationResources(): array
     {
         return [__DIR__ . '/../lang/'];
     }
